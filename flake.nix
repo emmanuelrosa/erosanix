1c6c16d7447b3d5d3fe6571f94eebf22ea3e0938
Emmanuel Rosa <emmanuelrosa@protonmail.com>
2021-12-14 14:19:45 -0500

    add fonts from my NUR

--- flake.nix
+++ flake.nix
@@ -8,18 +8,30 @@
       in {
         electrum-personal-server = callPackage ./pkgs/electrum-personal-server.nix {};
         nvidia-offload = callPackage ./pkgs/nvidia-offload.nix {};
+        century-gothic = callPackage ./pkgs/century-gothic {};
+        wingdings = callPackage ./pkgs/wingdings.nix {};
+        trace = callPackage ./pkgs/trace.nix {};
+        battery-icons = callPackage ./pkgs/battery-icons.nix {};
     };
 
     packages.aarch64-linux = let
       callPackage = nixpkgs.legacyPackages.aarch64-linux.callPackage;
       in {
         electrum-personal-server = callPackage ./pkgs/electrum-personal-server.nix {};
+        century-gothic = callPackage ./pkgs/century-gothic {};
+        wingdings = callPackage ./pkgs/wingdings.nix {};
+        trace = callPackage ./pkgs/trace.nix {};
+        battery-icons = callPackage ./pkgs/battery-icons.nix {};
     };
 
     packages.i686-linux = let
       callPackage = nixpkgs.legacyPackages.i686-linux.callPackage;
       in {
         electrum-personal-server = callPackage ./pkgs/electrum-personal-server.nix {};
+        century-gothic = callPackage ./pkgs/century-gothic {};
+        wingdings = callPackage ./pkgs/wingdings.nix {};
+        trace = callPackage ./pkgs/trace.nix {};
+        battery-icons = callPackage ./pkgs/battery-icons.nix {};
     };
 
     nixosModules.electrum-personal-server = import ./modules/electrum-personal-server.nix;
