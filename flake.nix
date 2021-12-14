b70c0b4e3d75beb17b08d630bb7bc7eb3af134d6
Emmanuel Rosa <emmanuelrosa@protonmail.com>
2021-12-14 14:47:25 -0500

    add more packages from my NUR
    I left out:
    
     - bisq-desktop: It's already in Nixpkgs
     - sierra-chart: I want to redo this package

--- flake.nix
+++ flake.nix
@@ -5,6 +5,7 @@
 
     packages.x86_64-linux = let
       callPackage = nixpkgs.legacyPackages.x86_64-linux.callPackage;
+      hsCallPackage = nixpkgs.legacyPackages.x86_64-linux.haskellPackages.callPackage;
       in {
         electrum-personal-server = callPackage ./pkgs/electrum-personal-server.nix {};
         nvidia-offload = callPackage ./pkgs/nvidia-offload.nix {};
@@ -12,26 +13,44 @@
         wingdings = callPackage ./pkgs/wingdings.nix {};
         trace = callPackage ./pkgs/trace.nix {};
         battery-icons = callPackage ./pkgs/battery-icons.nix {};
+        er-wallpaper = hsCallPackage ./pkgs/er-wallpaper.nix { };
+        pdf2png = callPackage ./pkgs/pdf2png.nix {};
+        rofi-menu = callPackage ./pkgs/rofi-menu.nix {};
+        bitcoin-onion-nodes = callPackage ./pkgs/bitcoin-onion-nodes.nix {};
+        sparrow = callPackage ./pkgs/sparrow.nix {};
+        muun-recovery-tool = callPackage ./pkgs/muun-recovery-tool.nix {};
+        tastyworks = callPackage ./pkgs/tastyworks.nix {};
     };
 
     packages.aarch64-linux = let
       callPackage = nixpkgs.legacyPackages.aarch64-linux.callPackage;
+      hsCallPackage = nixpkgs.legacyPackages.x86_64-linux.haskellPackages.callPackage;
       in {
         electrum-personal-server = callPackage ./pkgs/electrum-personal-server.nix {};
         century-gothic = callPackage ./pkgs/century-gothic {};
         wingdings = callPackage ./pkgs/wingdings.nix {};
         trace = callPackage ./pkgs/trace.nix {};
         battery-icons = callPackage ./pkgs/battery-icons.nix {};
+        er-wallpaper = hsCallPackage ./pkgs/er-wallpaper.nix { };
+        pdf2png = callPackage ./pkgs/pdf2png.nix {};
+        rofi-menu = callPackage ./pkgs/rofi-menu.nix {};
+        bitcoin-onion-nodes = callPackage ./pkgs/bitcoin-onion-nodes.nix {};
     };
 
     packages.i686-linux = let
       callPackage = nixpkgs.legacyPackages.i686-linux.callPackage;
+      hsCallPackage = nixpkgs.legacyPackages.x86_64-linux.haskellPackages.callPackage;
       in {
         electrum-personal-server = callPackage ./pkgs/electrum-personal-server.nix {};
         century-gothic = callPackage ./pkgs/century-gothic {};
         wingdings = callPackage ./pkgs/wingdings.nix {};
         trace = callPackage ./pkgs/trace.nix {};
         battery-icons = callPackage ./pkgs/battery-icons.nix {};
+        er-wallpaper = hsCallPackage ./pkgs/er-wallpaper.nix { };
+        pdf2png = callPackage ./pkgs/pdf2png.nix {};
+        rofi-menu = callPackage ./pkgs/rofi-menu.nix {};
+        bitcoin-onion-nodes = callPackage ./pkgs/bitcoin-onion-nodes.nix {};
+        muun-recovery-tool = callPackage ./pkgs/muun-recovery-tool.nix {};
     };
 
     nixosModules.electrum-personal-server = import ./modules/electrum-personal-server.nix;
