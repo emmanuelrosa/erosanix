3fbce79ace4405f0e36c16a7925d7d1688fb9ff3
Emmanuel Rosa <emmanuelrosa@protonmail.com>
2022-09-08 09:19:43 -0400

    updater: fix foobar2000 updater to handle beta releases

--- updaters/foobar2000.nix
+++ updaters/foobar2000.nix
@@ -6,7 +6,7 @@ libupdate.mkUpdateScript {
   derivation = builtins.toPath ../pkgs/foobar2000.nix;
 
   getRemoteVersion = ''
-      echo $(basename $(${pkgs.curl}/bin/curl -s https://www.foobar2000.org/download | ${pkgs.htmlq}/bin/htmlq -a href a | ${pkgs.gnugrep}/bin/grep getfile) | ${pkgs.gnused}/bin/sed 's/^foobar2000_v\([[:digit:]]\+\.[[:digit:]]\+\.[[:digit:]]\+\)\(.*\)/\1/')
+      echo $(basename $(${pkgs.curl}/bin/curl -s https://www.foobar2000.org/download | ${pkgs.htmlq}/bin/htmlq -a href a | ${pkgs.gnugrep}/bin/grep getfile | head -n 1) | ${pkgs.gnused}/bin/sed 's/^foobar2000_v\([[:digit:]]\+\.[[:digit:]]\+\.[[:digit:]]\+\)\(.*\)/\1/')
   '';
 
   getRemoteHash = libupdate.prefetchUrl "https://www.foobar2000.org/files/foobar2000_v$version.exe";
