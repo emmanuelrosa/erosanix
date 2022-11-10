0d9951d013659a2799c11777cabbc5a9a9369988
Emmanuel Rosa <emmanuelrosa@protonmail.com>
2022-11-10 11:34:21 -0500

    updaters: update roblox updater to get upstream version

--- updaters/roblox.nix
+++ updaters/roblox.nix
@@ -2,11 +2,11 @@
 , libupdate
 }:
 libupdate.mkUpdateScript {
-  comparator = "hash";
+  comparator = "version";
   derivation = builtins.toPath ../pkgs/roblox.nix;
 
   getRemoteVersion = ''
-    echo "no-op"
+    ${pkgs.curl}/bin/curl https://s3.amazonaws.com/setup.roblox.com/version | ${pkgs.gnused}/bin/sed 's/^version-\(.*\)$/\1/'
   '';
 
   getRemoteHash = libupdate.prefetchUrl "https://setup.rbxcdn.com/RobloxPlayerLauncher.exe";
