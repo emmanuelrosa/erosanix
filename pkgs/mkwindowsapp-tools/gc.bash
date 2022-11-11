c9b2830d1932bc82a41f372feeea0151eb11be99
Emmanuel Rosa <emmanuelrosa@protonmail.com>
2022-11-11 02:06:39 -0500

    mkwindowsapp-tools: cleanup output and add mutable field
    This change rearranges the output so that it's cleaner, and adds a field
    to indicate whether the layer is known to be mutable or not. The mutable
    flag is to be used for a deduplicator.

--- pkgs/mkwindowsapp-tools/gc.bash
+++ pkgs/mkwindowsapp-tools/gc.bash
@@ -1,8 +1,17 @@
 #! /usr/bin/bash
 
+function print_layer_info() {
+  local status=$1
+  local id=$2
+  local size=$3
+  local mutable=$4
+
+  printf "%s\t%s\t%s\t%s\n" $id $size $mutable $status
+}
+
 CACHE_DIR="$HOME/.cache/mkWindowsApp"
 
-printf "ACTION\tSIZE\tLAYER ID\n"
+printf "LAYER ID                                                        \tSIZE\tMUTABLE\tACTION\n"
 
 # API 0
 for file in $(ls $CACHE_DIR/*.referrers 2> /dev/null)
@@ -21,10 +30,10 @@ do
 
   if [ -s "$temp_file" ]
   then
-    printf "keeping\t%s\t%s\n" "$size" "$filename"
+    print_layer_info "keeping" $filename $size "?"
     mv "$temp_file" "$file"
   else
-    printf "deleting\t%s\t%s\n" "$size" "$filename"
+    print_layer_info "deleting" $filename $size "?"
     rm -fR "$CACHE_DIR/$filename"
     rm "$file"
     rm "$temp_file"
@@ -41,6 +50,8 @@ do
       filename="$(basename $layer)"
       size=$(du -h -s "$CACHE_DIR/$filename" | cut -f 1)
       temp_file=$(mktemp --suffix .mkwindowsapp-gc)
+      mutable="?"
+      [[ -f $layer/mutable ]] && mutable=$(cat $layer/mutable)
 
       for ref_path in $(cat $layer/refs)
       do
@@ -52,10 +63,10 @@ do
 
       if [ -s "$temp_file" ]
       then
-        printf "keeping\t%s\t%s\n" "$size" "$filename"
+        print_layer_info "keeping" $filename $size $mutable
         mv "$temp_file" "$layer/refs"
       else
-        printf "deleting\t%s\t%s\n" "$size" "$filename" 
+        print_layer_info "deleting" $filename $size $mutable
         rm -fR "$layer"
         rm "$temp_file"
       fi
