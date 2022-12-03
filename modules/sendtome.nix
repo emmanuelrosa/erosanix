1a090b0e403bc2577830a34b5ac8f1d9197511d5
Emmanuel Rosa <emmanuelrosa@protonmail.com>
2022-12-03 10:11:47 -0500

    sendtome: reimplement to address problems

--- modules/sendtome.nix
+++ modules/sendtome.nix
@@ -11,32 +11,91 @@ in {
         default = "";
         description = "The user to deliver mail to. Must be defined in users.users.";
       };
+
+      spoolDir = lib.mkOption {
+        type = lib.types.path;
+        default = "/var/spool/sendtome";
+        description = "The top-level directory where email messages are stored prior to delivery.";
+      };
+
+      deliveryInterval = lib.mkOption {
+        type = lib.types.str;
+        default = "10m";
+        description = "How frequently to check for queued emails and deliver them.";
+      };
     };
   };
 
   config = let
     userCfg = config.users.users."${cfg.user}";
+
+    prep = pkgs.writeScript "sendtome-prep" ''
+      chown -v ${cfg.user}:${userCfg.group} ${cfg.spoolDir}/new/*
+    '';
+
+    deliver = pkgs.writeScript "sendtome-deliver" ''
+      MAILDIR=${userCfg.home}/Maildir
+
+      mkdir -p $MAILDIR/new
+      mkdir -p $MAILDIR/cur
+
+      mv -v ${cfg.spoolDir}/new/* $MAILDIR/new/
+    '';
   in lib.mkIf cfg.setSendmail {
+    systemd.tmpfiles.rules = [
+      "d /${cfg.spoolDir} 755 root root -"
+      "d /${cfg.spoolDir}/new 1777 root root -"
+      "d /${cfg.spoolDir}/tmp 1777 root root -"
+    ];
+
+    systemd.services.sendtome = {
+      description = "sendtome delivery service";
+      after = [ "network.target" ];
+      wantedBy = [ "multi-user.target" ];
+
+      serviceConfig = {
+        Type = "oneshot";
+        User = cfg.user;
+        Group = userCfg.group;
+
+        ExecStartPre = "+${prep}";
+        ExecStart = "${deliver}";
+      };
+    };
+
+    systemd.timers.sendtome = {
+      description = "Timer to deliver email via sendtome.";
+      wantedBy = [ "timers.target" ];
+      timerConfig = {
+        OnUnitActiveSec="${cfg.deliveryInterval}";
+      };
+    };
+
     services.mail.sendmailSetuidWrapper = {
-      setuid = true;
-      setgid = true;
-      program = "sendmail";
       owner = cfg.user;
       group = userCfg.group;
+      program = "sendmail";
       source = pkgs.writeScript "sendmail" ''
         #! ${pkgs.bash}/bin/bash
         # Based on a script from https://unix.stackexchange.com/questions/82093/minimal-mta-that-delivers-mail-locally-for-cron
 
-        MAILDIR=${userCfg.home}/Maildir
         rand=$((RANDOM % 1000))
         msgname=$(date +%s).P$$R$rand.$(hostname | tr '/:' '\057\072')
+        tmp_msgpath=${cfg.spoolDir}/tmp/$msgname
 
-        mkdir -p $MAILDIR/new
-        mkdir -p $MAILDIR/tmp
-        mkdir -p $MAILDIR/cur
+        cat > $tmp_msgpath
 
-        cat > $MAILDIR/tmp/$msgname
-        mv -n $MAILDIR/tmp/$msgname $MAILDIR/new/$msgname
+        # If the encoding is base64, decode into plain text
+        if [ "$(grep 'Content-Transfer-Encoding: base64' $tmp_msgpath)" != "" ] 
+        then 
+          tmp_message=$(mktemp)
+          sed -e '/^$/,$d' < $tmp_msgpath > $tmp_message
+          echo "" >> $tmp_message
+          sed -e '1,/^To\:/d' < $tmp_msgpath | base64 -d >> $tmp_message
+          mv $tmp_message $tmp_msgpath
+        fi
+
+        mv -n $tmp_msgpath ${cfg.spoolDir}/new/$msgname
       '';
     };
   };
