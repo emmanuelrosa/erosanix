da793a0aa04cf051642eeb49d4af265b8db2f0ce
Emmanuel Rosa <emmanuelrosa@protonmail.com>
2022-12-22 13:45:50 -0500

    epic-games-launcher: create 'Epic Games' directory
    This is to ensure games are installed there from the start.

--- pkgs/epic-games-launcher.nix
+++ pkgs/epic-games-launcher.nix
@@ -36,6 +36,7 @@ mkWindowsApp rec {
 
   winAppInstall = ''
     ${zenity}/bin/zenity --info --text "Epic Games Launcher takes a LONG time to install, and there's little visual indication of progress. Sit back, relax, and be ready to confirm the installation of C++ runtimes; There are two of them. You may be able to see what's going on with journalctl."
+    mkdir -p "$HOME/Games/Epic Games"
     winetricks -q arial cjkfonts vcrun2019 d3dcompiler_43 d3dcompiler_47 d3dx9 dotnet471
     ${dxvk}/bin/setup_dxvk.sh install
 
