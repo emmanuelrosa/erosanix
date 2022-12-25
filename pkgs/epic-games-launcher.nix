d6dce612a2f767658d7dd678a535d676f77c9017
Emmanuel Rosa <emmanuelrosa@protonmail.com>
2022-12-25 10:52:36 -0500

    epic-games-launcher: remove various winetricks

--- pkgs/epic-games-launcher.nix
+++ pkgs/epic-games-launcher.nix
@@ -34,9 +34,9 @@ mkWindowsApp rec {
   };
 
   winAppInstall = ''
-    ${zenity}/bin/zenity --info --text "Epic Games Launcher takes a LONG time to install, and there's little visual indication of progress. Sit back, relax, and be ready to confirm the installation of C++ runtimes; There are two of them. You may be able to see what's going on with journalctl."
+    ${zenity}/bin/zenity --info --text "Epic Games Launcher takes a LONG time to install, and there's little visual indication of progress. You can see what's going on with journalctl."
     mkdir -p "$HOME/Games/Epic Games"
-    winetricks -q arial cjkfonts vcrun2019 d3dcompiler_43 d3dcompiler_47 d3dx9 dotnet471
+    winetricks -q d3dcompiler_43 d3dcompiler_47 d3dx9
     ${dxvk}/bin/setup_dxvk.sh install
 
     rm $WINEPREFIX/drive_c/users/$USER/Desktop
