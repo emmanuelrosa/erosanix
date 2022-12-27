02743406544a6be0876a3c03a6816942afca7d9f
Emmanuel Rosa <emmanuelrosa@protonmail.com>
2022-12-27 14:07:36 -0500

    nvidia-offload: export NVIDIA ICD file
    Something must have changed recently and offload rendering with Vulkan
    had stopped working.
    
    This appears to fix it.

--- pkgs/nvidia-offload.nix
+++ pkgs/nvidia-offload.nix
@@ -1,4 +1,5 @@
 { writeShellScriptBin,
+  findutils,
   provider ? "NVIDIA-G0",
   glxVendor ? "nvidia",
   vkLayer ? "NVIDIA_only"
@@ -8,5 +9,14 @@ writeShellScriptBin "nvidia-offload" ''
   export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=${provider}
   export __GLX_VENDOR_LIBRARY_NAME=${glxVendor}
   export __VK_LAYER_NV_optimus=${vkLayer}
+
+  if [ -e /etc/NIXOS ]
+  then
+    export VK_ICD_FILENAMES=$(${findutils}/bin/find /run/opengl-driver/share/vulkan/icd.d -name nvidia_icd*)
+  else
+    export VK_ICD_FILENAMES=$(${findutils}/bin/find /usr/share/vulkan/icd.d -name nvidia_icd*)
+  fi
+
+  echo "nvidia-offload: Using NVIDIA ICD $VK_ICD_FILENAMES"
   exec -a "$0" "$@"
 ''
