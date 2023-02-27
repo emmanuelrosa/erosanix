0764fc01d83f3119a5f117e4cbae06022dffabb2
Emmanuel Rosa <emmanuelrosa@protonmail.com>
2023-02-27 05:26:32 -0500

    nvidia-offload: set libva and pdpau drivers

--- pkgs/nvidia-offload.nix
+++ pkgs/nvidia-offload.nix
@@ -2,13 +2,17 @@
   findutils,
   provider ? "NVIDIA-G0",
   glxVendor ? "nvidia",
-  vkLayer ? "NVIDIA_only"
+  vkLayer ? "NVIDIA_only",
+  libVaDriver ? "vdpau",
+  vdpauDriver ? "nvidia"
 }:
 writeShellScriptBin "nvidia-offload" ''
   export __NV_PRIME_RENDER_OFFLOAD=1
   export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=${provider}
   export __GLX_VENDOR_LIBRARY_NAME=${glxVendor}
   export __VK_LAYER_NV_optimus=${vkLayer}
+  export LIBVA_DRIVER_NAME="${libVaDriver}"
+  export VDPAU_DRIVER="${vdpauDriver}"
 
   if [ -e /etc/NIXOS ]
   then
