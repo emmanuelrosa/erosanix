b9578c945d73033bf3bb378b54d4a70f3e7d3d37
Emmanuel Rosa <emmanuelrosa@protonmail.com>
2023-04-27 16:03:29 -0400

    onlyoffice: only symlink icons if they exist
    This reverts commit fc38337e5e6cc9ef3a55c33780eca362f593d6b4.

--- modules/onlyoffice.nix
+++ modules/onlyoffice.nix
@@ -31,7 +31,7 @@ let
 
     extraInstallCommands = ''
       mkdir -p $out/share/applications
-      cp -r ${onlyofficeUnwrapped}/share/icons $out/share
+      test -d ${onlyofficeUnwrapped}/share/icons && ln -s ${onlyofficeUnwrapped}/share/icons $out/share
       cp ${onlyofficeUnwrapped}/share/applications/onlyoffice-desktopeditors.desktop $out/share/applications
       substituteInPlace $out/share/applications/onlyoffice-desktopeditors.desktop \
         --replace "${onlyofficeUnwrapped}/bin/DesktopEditors" "$out/bin/onlyoffice"
