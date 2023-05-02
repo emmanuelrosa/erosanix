e7e365d56a9618cb84f80782a5274d247fe48f3d
Emmanuel Rosa <emmanuelrosa@protonmail.com>
2023-05-02 12:16:26 -0400

    updater: factor out the multi-architecture updater

--- update.nix
+++ update.nix
@@ -107,12 +107,30 @@ let
     rm $updated_nix_src
   '';
 
+  multiArchDerivationUpdater = let
+    updateDerivation = pkgs.writeText "update-derivation.awk" ''
+      /#:version:/ { match ($0, /^(.*)"(.+)"(.*)/, m); printf ("%s\"%s\"%s\n", m[1], version , m[3]) }
+      /#:hash64:/ { match ($0, /^(.*)"([0-9a-z-]+)"(.*)/, m); printf ("%s\"%s\"%s\n", m[1], hash64 , m[3]) }
+      /#:hash32:/ { match ($0, /^(.*)"([0-9a-z-]+)"(.*)/, m); printf ("%s\"%s\"%s\n", m[1], hash32 , m[3]) }
+      !/#:version:/  && !/#:hash64:/ && !/#:hash32:/ { print $0 }
+    '';
+  in '' 
+    hash64=$(echo $hash | cut -d "|" -f 1)
+    hash32=$(echo $hash | cut -d "|" -f 2)
+    echo "hash64: $hash64"
+    echo "hash32: $hash32"
+    updated_nix_src=$(mktemp)
+    ${pkgs.gawk}/bin/awk -f ${updateDerivation} -v version=$version -v hash64=$hash64 -v hash32=$hash32 $derivation > $updated_nix_src
+    cat $updated_nix_src > $derivation
+    rm $updated_nix_src
+  '';
+
   importUpdater = updater:
     import updater { 
       inherit pkgs;
 
       libupdate = {
-        inherit mkUpdateScript prefetchUrl getRemoteVersionFromGitHub;
+        inherit mkUpdateScript prefetchUrl getRemoteVersionFromGitHub multiArchDerivationUpdater;
       };
     };
 
--- updaters/notepad++.nix
+++ updaters/notepad++.nix
@@ -17,21 +17,5 @@ libupdate.mkUpdateScript {
     echo "$hash64|$hash32"
     '';
 
-  derivationUpdater = let
-    updateDerivation = pkgs.writeText "update-derivation.awk" ''
-      /#:version:/ { match ($0, /^(.*)"(.+)"(.*)/, m); printf ("%s\"%s\"%s\n", m[1], version , m[3]) }
-      /#:hash64:/ { match ($0, /^(.*)"([0-9a-z-]+)"(.*)/, m); printf ("%s\"%s\"%s\n", m[1], hash64 , m[3]) }
-      /#:hash32:/ { match ($0, /^(.*)"([0-9a-z-]+)"(.*)/, m); printf ("%s\"%s\"%s\n", m[1], hash32 , m[3]) }
-      !/#:version:/  && !/#:hash64:/ && !/#:hash32:/ { print $0 }
-    '';
-  in '' 
-    hash64=$(echo $hash | cut -d "|" -f 1)
-    hash32=$(echo $hash | cut -d "|" -f 2)
-    echo "hash64: $hash64"
-    echo "hash32: $hash32"
-    updated_nix_src=$(mktemp)
-    ${pkgs.gawk}/bin/awk -f ${updateDerivation} -v version=$version -v hash64=$hash64 -v hash32=$hash32 $derivation > $updated_nix_src
-    cat $updated_nix_src > $derivation
-    rm $updated_nix_src
-  '';
+  derivationUpdater = libupdate.multiArchDerivationUpdater;
 }
