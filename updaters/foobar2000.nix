b1d819701dacb27d24c9deb6d62484e432da5667
Emmanuel Rosa <emmanuelrosa@protonmail.com>
2023-05-02 12:21:30 -0400

    updater: fix foobar2000 updater

--- updaters/foobar2000.nix
+++ updaters/foobar2000.nix
@@ -6,8 +6,14 @@ libupdate.mkUpdateScript {
   derivation = builtins.toPath ../pkgs/foobar2000.nix;
 
   getRemoteVersion = ''
-      echo $(basename $(${pkgs.curl}/bin/curl -s https://www.foobar2000.org/download | ${pkgs.htmlq}/bin/htmlq -a href a | ${pkgs.gnugrep}/bin/grep getfile | head -n 1) | ${pkgs.gnused}/bin/sed 's/^foobar2000_v\([[:digit:]]\+\.[[:digit:]]\+\.[[:digit:]]\+\)\(.*\)/\1/')
+      echo $(basename $(${pkgs.curl}/bin/curl -s https://www.foobar2000.org/download | ${pkgs.htmlq}/bin/htmlq -a href a | ${pkgs.gnugrep}/bin/grep getfile | head -n 1) | ${pkgs.gnused}/bin/sed 's/^foobar2000_v\(.*\)\(.exe\)/\1/')
   '';
 
-  getRemoteHash = libupdate.prefetchUrl "https://www.foobar2000.org/files/foobar2000_v$version.exe";
+  getRemoteHash = ''
+    hash64=$(nix-prefetch-url --type sha256 "https://www.foobar2000.org/files/foobar2000-x64_v$version.exe")
+    hash32=$(nix-prefetch-url --type sha256 "https://www.foobar2000.org/files/foobar2000_v$version.exe")
+    echo "$hash64|$hash32"
+    '';
+
+  derivationUpdater = libupdate.multiArchDerivationUpdater;
 }
