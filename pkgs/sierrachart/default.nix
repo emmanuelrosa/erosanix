017f542daa25e3db439d264e9f038827140df780
Emmanuel Rosa <emmanuelrosa@protonmail.com>
2023-07-22 10:03:43 -0400

    sierrachart: 2525 -> 2523
    Downgrading sierrachart due to issues with 2524 and 2525 on Linux.
    See https://www.sierrachart.com/SupportBoard.php?ThreadID=85248

--- pkgs/sierrachart/default.nix
+++ pkgs/sierrachart/default.nix
@@ -12,10 +12,10 @@
 , studies ? []
 , symlinkJoin
 }: let
-  version = "2525"; #:version:
+  version = "2523"; #:version:
   src = fetchurl {
     url = "https://www.sierrachart.com/downloads/ZipFiles/SierraChart${version}.zip";
-    sha256 = "0l2wy6qwg9j3xlkrh795h41g0iiaxdf8wsxgrsj7h2vqdq4ppvg9"; #:hash:
+    sha256 = "0iv799ywm6qhvvad8v5s4799n35glqx3zhn3k768ngxa4wgmccjn"; #:hash:
   };
 
   defaultStudies = {
