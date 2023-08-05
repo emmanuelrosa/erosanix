2e5d02d5d2dab72700c1e54bf577615803932d18
Emmanuel Rosa <emmanuelrosa@protonmail.com>
2023-08-05 13:21:44 -0400

    sierrachart: 2532 -> 2533

--- pkgs/sierrachart/default.nix
+++ pkgs/sierrachart/default.nix
@@ -12,10 +12,10 @@
 , studies ? []
 , symlinkJoin
 }: let
-  version = "2532"; #:version:
+  version = "2533"; #:version:
   src = fetchurl {
     url = "https://www.sierrachart.com/downloads/ZipFiles/SierraChart${version}.zip";
-    sha256 = "00n43zm0hp60asqvi65na13y6k1qfp2k74ywn3m5yajimjq95ci2"; #:hash:
+    sha256 = "0ixgbiqsn77fmsdp0byyw3f267wvdms1gi4ksri10sy8jz8svmsf"; #:hash:
   };
 
   defaultStudies = {
