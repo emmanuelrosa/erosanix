0090bc4b20cbefbe328f698bda802138b4142939
Emmanuel Rosa <emmanuelrosa@protonmail.com>
2023-12-21 13:09:36 -0500

    implement composeAndApply using compose

--- lib/trivial.nix
+++ lib/trivial.nix
@@ -1,4 +1,4 @@
-{ 
+rec { 
   # Returns a function which when applied to an argument,
   # applies the functions in the list, in the order provided.
   # `(compose [ f1 f2 f3 ]) arg` is the same as `f3 (f2 (f1 arg)`
@@ -10,5 +10,5 @@
   # Composes a list of functions (see `compose` above) and then
   # applies them to the argument.
   # `composeAndApply [ f1 f2 f3 ] arg` is the same as `f3 (f2 (f1 arg)`
-  composeAndApply = xs: arg: builtins.foldl' (x: f: f x) arg xs;
+  composeAndApply = xs: arg: (compose xs) arg;
 }
