bf1bff1ee08a600a6421710f2585633987bc1c48
Emmanuel Rosa <emmanuelrosa@protonmail.com>
2023-12-29 15:30:34 -0500

    lib.mkmupen64plus: add default icon

--- lib/mkmupen64plus.nix
+++ lib/mkmupen64plus.nix
@@ -5,16 +5,19 @@
 , makeDesktopIcon
 , copyDesktopItems
 , copyDesktopIcons
+, fetchurl
 }:
 { name                          # The name of the executable.
 , desktopName                   # The name displayed in the "applications" menu.
 , category ? "Emulator"         # The freedesktop.org additional category.
                                 # Can be any additional category where the "related category" is "Game". Defaults to "Emulator".
                                 # See https://specifications.freedesktop.org/menu-spec/latest/apas02.html
-, icon ? {}                     # Attribute set passed to makeDesktopIcon, minus the 'name" attribute:
-                                # { src - Build-time path to the icon image.
-                                #   icoIndex - For *.ico files, a number indicating which of the multiple images to extract; Etc, 0.
-                                # }
+, icon ? {                      # Attribute set passed to makeDesktopIcon, minus the 'name" attribute:
+  src = fetchurl {
+    url = "https://1000logos.net/wp-content/uploads/2017/07/Color-N64-Logo-500x393.jpg";
+    sha256 = "01hnfbrrgvb560m14hq4nwirhngl8mss6s6wmbl56bl7jnngzpig";
+  };
+}
 , rom                           # Run-time path to the N64 ROM file.
 }: 
 let
@@ -23,7 +26,7 @@ let
   '';
 in stdenv.mkDerivation rec {
   pname = "${name}-mupen64plus-wrapper";
-  version = "1.0.0";
+  version = "1.1.0";
   src = mupen64plus; # Dummy src; It's not used.
   dontUnpack = true;
   nativeBuildInputs = [ copyDesktopItems copyDesktopIcons ];
