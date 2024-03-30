f7f08338a6278212f7dcee0686952d7e9b2456b6
Emmanuel Rosa <emmanuelrosa@protonmail.com>
2024-03-30 15:47:20 -0400

    blockstream-green: 2.0.2 -> 2.0.3

--- pkgs/blockstream-green/default.nix
+++ pkgs/blockstream-green/default.nix
@@ -21,11 +21,11 @@
 , xcb-util-cursor
 }: stdenv.mkDerivation rec {
   pname = "blockstream-green";
-  version = "2.0.2"; #:version:#
+  version = "2.0.3"; #:version:#
 
   src = fetchurl {
     url = "https://github.com/Blockstream/green_qt/releases/download/release_${version}/BlockstreamGreen-Linux-x86_64.tar.gz";
-    sha256 = "1fkjn4qsjd5wh91mfd9awzdiqlmv4avn9y06pzy1j2c8ip5a049l"; #:hash:
+    sha256 = "1djln5gfr8rmhddf1ifn240jzfs553rsn9cg7hdjjrf6f6d9flgc"; #:hash:
   };
 
   setSourceRoot = ''
