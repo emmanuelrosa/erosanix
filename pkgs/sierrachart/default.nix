2d06ca5981df856948cf21ead82463f193aba651
Emmanuel Rosa <emmanuelrosa@protonmail.com>
2024-12-17 15:35:08 -0500

    sierrachart: 2721 -> 2722

--- pkgs/sierrachart/default.nix
+++ pkgs/sierrachart/default.nix
@@ -15,7 +15,7 @@
 , msvcShim
 , enableCompilerShim ? false
 }: let
-  version = "2721"; #:version:
+  version = "2722"; #:version:
   src = fetchurl {
     urls = [
       "https://download1.sierrachart.com/downloads/ZipFiles/SierraChart${version}.zip"
