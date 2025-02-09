60909cb281af2ac23df3a7df49299f80fcea67af
Emmanuel Rosa <emmanuelrosa@protonmail.com>
2025-02-09 13:14:55 -0500

    fix swaync module

--- modules/swaync.nix
+++ modules/swaync.nix
@@ -14,6 +14,21 @@ in {
 
   config = lib.mkIf config.programs.swaynotificationcenter.enable {
     environment.systemPackages = [ cfg.package ];
-    systemd.packages = [ cfg.package ];
+    systemd.user.services.swaync = {
+      enable = true;
+      description = "Sway notificiation center daemon";
+      documentation = [ "https://github.com/ErikReider/SwayNotificationCenter" ];
+      after = [ "graphical-session.target" ];
+      wantedBy = [ "graphical-session.target" ];
+      partOf = [ "graphical-session.target" ];
+      serviceConfig = {
+        Type = "dbus";
+        BusName = "org.freedesktop.Notifications";
+        Slice = "background-graphical.slice";
+        Restart = "on-failure";
+        ExecStart = "${cfg.package}/bin/swaync";
+        ExecReload = "${cfg.package}/bin/swaync-client --reload-config; ${cfg.package}/bin/swaync-client --reload-css";
+      };
+    };
   };
 }
