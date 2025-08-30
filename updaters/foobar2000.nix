f28776c49ddb4d34abc01092009fba0cd96836bd
Emmanuel Rosa <emmanuelrosa@protonmail.com>
2025-08-30 18:32:09 -0400

    update foobar2000 updater

--- updaters/foobar2000.nix
+++ updaters/foobar2000.nix
@@ -6,12 +6,12 @@ libupdate.mkUpdateScript {
   derivation = builtins.toPath ../pkgs/foobar2000.nix;
 
   getRemoteVersion = ''
-      echo $(basename $(${pkgs.curl}/bin/curl -s https://www.foobar2000.org/download | ${pkgs.htmlq}/bin/htmlq -a href a | ${pkgs.gnugrep}/bin/grep getfile | head -n 1) | ${pkgs.gnused}/bin/sed 's/^foobar2000_v\(.*\)\(.exe\)/\1/')
+      echo $(basename $(${pkgs.curl}/bin/curl -s https://www.foobar2000.org/windows | ${pkgs.htmlq}/bin/htmlq -a href a | ${pkgs.gnugrep}/bin/grep -v preview | ${pkgs.gnugrep}/bin/grep downloads | head -n 1) | ${pkgs.gnused}/bin/sed -e 's/.*foobar2000_v//g' -e 's/\.exe//g')
   '';
 
   getRemoteHash = ''
-    hash64=$(nix-prefetch-url --type sha256 "https://www.foobar2000.org/files/foobar2000-x64_v$version.exe")
-    hash32=$(nix-prefetch-url --type sha256 "https://www.foobar2000.org/files/foobar2000_v$version.exe")
+    hash64=$(nix-prefetch-url --type sha256 "https://www.foobar2000.org/downloads/foobar2000-x64_v$version.exe")
+    hash32=$(nix-prefetch-url --type sha256 "https://www.foobar2000.org/downloads/foobar2000_v$version.exe")
     echo "$hash64|$hash32"
     '';
 
