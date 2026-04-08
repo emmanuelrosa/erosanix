f2c30775b4e64b441372fa5d8f2cd6300f5be1fe
Emmanuel Rosa <emmanuelrosa@protonmail.com>
2026-04-08 11:10:54 -0400

    add warning about the future removal of the blockstream package

--- flake.nix
+++ flake.nix
@@ -179,7 +179,7 @@
 
         blockstream-green = pkgs.lib.warn "blockstream-green has been renamed to blockstream" self.packages.x86_64-linux.blockstream;
 
-        blockstream = callPackage ./pkgs/blockstream/default.nix { };
+        blockstream = pkgs.lib.warn "The blockstream package will be removed because it has proved to be unreliable." callPackage ./pkgs/blockstream/default.nix { };
 
         gossip = callPackage ./pkgs/gossip/default.nix { 
           inherit (lib) makeDesktopIcon copyDesktopIcons;
