3fc07ae67fc4fbc0f3630b24273c31c5c66ccb40
Emmanuel Rosa <emmanuelrosa@protonmail.com>
2026-04-15 07:34:26 -0400

    nanogl: re-write
    A step toward resolving #30

--- lib/nanogl.nix
+++ lib/nanogl.nix
@@ -1,27 +1,63 @@
 { lib
+, symlinkJoin
 , writeShellScript
 , runCommand
 , genericBinWrapper
 , mesa
+, linuxPackages
 , libglvnd
 , libvdpau-va-gl
 }:
-pkg: vadrivers:
+{ package
+, vadrivers ? []
+, enableNvidia ? false
+}:
 let
-  mesa-drivers = [ mesa.drivers ];
-  libvdpau = [ libvdpau-va-gl ];
+  mesa-drivers = [ mesa ];
+  nvidia_x11 = linuxPackages.nvidia_x11;
 
   glxindirect = runCommand "mesa_glxindirect" { } (''
     mkdir -p $out/lib
-    ln -s ${mesa.drivers}/lib/libGLX_mesa.so.0 $out/lib/libGLX_indirect.so.0
+    ln -s ${mesa}/lib/libGLX_mesa.so.0 $out/lib/libGLX_indirect.so.0
   '');
 
+  libraryPath = builtins.concatStringsSep ":" [
+    (symlinkJoin {
+      name = "nanogl-libraries";
+      paths = [
+        "${mesa}/lib"
+        "${mesa}/lib/dri"
+        "${mesa}/lib/gbm"
+        "${libvdpau-va-gl}/lib/vdpau"
+        "${libglvnd}/lib"
+        "${glxindirect}/lib"
+      ] ++ (if enableNvidia then [
+        "${nvidia_x11}/lib"
+        "${nvidia_x11}/lib/gbm"
+      ] else []);
+    })
+  ];
+
+  gbmDriversPath = builtins.concatStringsSep ":" ([
+    "${mesa}/lib/gbm"
+  ] ++ (if enableNvidia then [
+    (lib.optionalString enableNvidia "${nvidia_x11}/lib/gbm")
+  ] else []));
+
+  eglVendorLibraryPath = builtins.concatStringsSep ":" ([
+    "${mesa}/share/glvnd/egl_vendor.d/"
+  ] ++ (if enableNvidia then [
+    (lib.optionalString enableNvidia "${nvidia_x11}/share/glvnd/egl_vendor.d/")
+  ] else []));
+
   wrapper = writeShellScript "nanogl-wrapper" ''
-    export LIBGL_DRIVERS_PATH=${lib.makeSearchPathOutput "lib" "lib/dri" mesa-drivers}
     export LIBVA_DRIVERS_PATH=${lib.makeSearchPathOutput "out" "lib/dri" (mesa-drivers ++ vadrivers)}
-    ${''export __EGL_VENDOR_LIBRARY_FILENAMES=${mesa.drivers}/share/glvnd/egl_vendor.d/50_mesa.json"''${__EGL_VENDOR_LIBRARY_FILENAMES:+:$__EGL_VENDOR_LIBRARY_FILENAMES}"''
-    }
-    export LD_LIBRARY_PATH=${lib.makeLibraryPath mesa-drivers}:${lib.makeSearchPathOutput "lib" "lib/vdpau" libvdpau}:${glxindirect}/lib:${lib.makeLibraryPath [libglvnd]}"''${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}"
+    export LIBGL_DRIVERS_PATH=${libraryPath}
+    export GBM_BACKENDS_PATH=${gbmDriversPath}
+    export LD_LIBRARY_PATH=${libraryPath}
+    export __EGL_VENDOR_LIBRARY_DIRS=${eglVendorLibraryPath}
+
     exec "@EXECUTABLE@" "$@"
   '';
-in genericBinWrapper pkg wrapper
+in genericBinWrapper package wrapper
+
